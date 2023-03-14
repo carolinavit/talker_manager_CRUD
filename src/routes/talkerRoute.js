af4107ce9ed2d564b7d4305ceb25494c5d9f9ce7
Vitória <vitcs11@gmail.com>
2023-03-14 20:56:26 -0300

    Requisito 2

--- src/routes/talkerRoute.js
+++ src/routes/talkerRoute.js
@@ -6,9 +6,21 @@ const path = require('path');
 
 const readFile = require('../utils/readFile');
 
+const TALKER_DATA_PATH = path.resolve(__dirname, '../talker.json');
+
 talkerRoute.get('/', async (_req, res) => {
-    const data = await readFile(path.resolve(__dirname, '../talker.json'));
+    const data = await readFile(TALKER_DATA_PATH);
     return res.status(200).json(data);
+});
+
+talkerRoute.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const data = await readFile(TALKER_DATA_PATH);
+  const filterId = data.find((e) => e.id === +(id));
+  if (!filterId) {
+    return res.status(404).send({ message: 'Pessoa palestrante não encontrada' });
+  }
+  return res.status(200).json(filterId);
 });
 
 module.exports = talkerRoute;
